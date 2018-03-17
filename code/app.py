08f7454d2d402c830b9be7b3e41b5c400215cb1b
gurdipsingh <gurdipsingh1990@gmail.com>
2018-03-17 15:24:24 -0700

    Added simple get route

--- code/app.py
+++ code/app.py
@@ -1,5 +1,13 @@
-from flask import flask
-from flask_restful import Resouce, Api
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-app = Api(app)
+api = Api(app)
+
+class Students(Resource):
+    def get(self, name):
+        return { 'student': name }
+
+api.add_resource(Students, '/student/<string:name>')
+
+app.run(port = 4000)
