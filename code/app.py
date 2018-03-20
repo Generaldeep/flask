fc15028fe9ee036e2069cd4b76b1ce3eadc00fee
gurdipsingh <gurdipsingh1990@gmail.com>
2018-03-20 14:58:52 -0700

    Moved parser args to be used with Item class instead of each request

--- code/app.py
+++ code/app.py
@@ -13,6 +13,14 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type = float,
+        required = True,
+        help = 'This field cannot be left blank'
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -22,15 +30,7 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'item': 'item with name {} already exists'.format(name)}, 400
 
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            'price',
-            type = float,
-            required = True,
-            help = 'This field cannot be left blank'
-        )
-
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
 
         item = {'name': name, 'price': data['price']}
         items.append(item)
@@ -42,15 +42,7 @@ class Item(Resource):
         return {'message': 'item has been deleted'}
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            'price',
-            type = float,
-            required = True,
-            help = 'This field cannot be left blank'
-        )
-
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
 
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
