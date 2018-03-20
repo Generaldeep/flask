c82559b4dafbf96165bf587f9d9c758c9d120e25
gurdipsingh <gurdipsingh1990@gmail.com>
2018-03-20 09:10:34 -0700

    Added jwt packagae, and in memory user in security.py

--- code/security.py
+++ code/security.py
@@ -1,7 +1,23 @@
     users = [
         {
             'id': 1,
-            'username': 'Deep',
-            'password': 'whatitdo!'
+            'username': 'deep',
+            'password': 'whatitdo'
         }
     ]
+
+username_mapping = {
+    'deep': {
+        'id': 1,
+        'username': 'deep',
+        'password': 'whatitdo'
+    }
+}
+
+userid_mapping = {
+    1: {
+        'id': 1,
+        'username': 'deep',
+        'password': 'whatitdo'
+    }
+}
