fd815b18ac5731324cb8f052e952165003dc8b75
gurdipsingh <gurdipsingh1990@gmail.com>
2018-03-20 16:20:05 -0700

    created simple users table with id, username, password as values

--- sqlite_db.py
+++ sqlite_db.py
@@ -6,3 +6,11 @@ cursor = connection.cursor()
 
 create_table = 'CREATE TABLE users (id int, username text, password text)'
 cursor.execute(create_table)
+
+user = (1, 'deep', 'blahblah')
+
+insert_query = 'INSERT INTO users VALUES (?, ?, ?)'
+cursor.execute(insert_query, user)
+
+connection.commit()
+connection.close()
