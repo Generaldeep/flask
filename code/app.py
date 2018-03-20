ea4158e8945094cb9e5676786057b961ca287921
gurdipsingh <gurdipsingh1990@gmail.com>
2018-03-20 14:53:49 -0700

    Refactored post method to use req.parser()

--- code/app.py
+++ code/app.py
@@ -22,7 +22,16 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'item': 'item with name {} already exists'.format(name)}, 400
 
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'price',
+            type = float,
+            required = True,
+            help = 'This field cannot be left blank'
+        )
+
+        data = parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
