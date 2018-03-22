d70ba96fdf98dab911068189d57a41ec8537309a
gurdipsingh <gurdipsingh1990@gmail.com>
2018-03-22 15:56:16 -0700

    Extracted the connection/insertion portion from post item method into its' own method, also added try/except for error handling

--- code/item.py
+++ code/item.py
@@ -42,6 +42,16 @@ class Item(Resource):
 
         item = {'name': name, 'price': data['price']}
 
+        try:
+            self.insert(item)
+        except:
+            return {'message': 'An error occuring trying to insert the item.'}, 500
+
+        return item, 201
+
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -51,7 +61,6 @@ class Item(Resource):
         connection.commit()
         connection.close()
 
-        return item, 201
 
 
     def delete(self, name):
